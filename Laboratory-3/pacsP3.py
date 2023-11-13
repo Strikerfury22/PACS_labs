3a748b75dc02c44d716e78458c3d8887a73d8b14
Strikerfury22 <71235406+Strikerfury22@users.noreply.github.com>
2023-11-13 18:59:15 +0100

    Fin p3

--- Laboratory-3/pacsP3.py
+++ Laboratory-3/pacsP3.py
@@ -7,33 +7,31 @@ standard_deviation = [7.549834435, 330.8353065, 469.3900297, 1.527525232, 94.690
 CV = [2.19972E-05, 0.00188435, 0.005292122, 3.44226E-05, 0.004252196]
 x_axis = [1, 2, 4, 8, 16]
 
-plt.plot(x_axis, test2, 'r', label = 'Test1')
-plt.plot(x_axis, test2, 'b', label = 'Test2')
-plt.plot(x_axis, test2, 'g', label = 'Test3')
-plt.ylabel('Time (milliseconds)')
-plt.xlabel('Number of Threads')
-plt.title('4294967295 step Testing times')
-plt.legend()
-plt.show()
 
-plt.plot(x_axis, mean, 'r', label = 'mean')
+plt.plot(x_axis, mean, 'r')
 plt.ylabel('Time (seconds)')
 plt.xlabel('Number of Threads')
 plt.title('Mean')
-plt.legend()
 plt.show()
 
-plt.plot(x_axis, standard_deviation, 'r', label = 'Standard Deviation')
+plt.plot(x_axis, standard_deviation, 'g')
 plt.ylabel('Deviation')
 plt.xlabel('Number of Threads')
-plt.title('Standard Deviation')
-plt.legend()
+plt.title('Standard deviation')
 plt.show()
 
-plt.plot(x_axis, CV, 'r', label = 'Coefficient of Variation')
+plt.plot(x_axis, CV, 'b', label = 'Coefficient of Variation')
 plt.ylabel('Coefficient')
 plt.xlabel('Number of Threads')
 plt.title('Coefficient of Variation')
+plt.show()
+
+plt.plot(x_axis, test1, 'r', label = 'Test1')
+plt.plot(x_axis, test2, 'b', label = 'Test2')
+plt.plot(x_axis, test3, 'g', label = 'Test3')
+plt.ylabel('Time (milliseconds)')
+plt.xlabel('Number of Threads')
+plt.title('4294967295 step Testing times')
 plt.legend()
 plt.show()
 
